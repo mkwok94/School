fefd595a6a7bf3c11becb70d7885aec4e7d07ddd
mkwok94 <m.kwok94@yahoo.com>
2016-07-13 22:07:01 -0700

    HW code

---
+++ mortgageCalculator2.cpp
@@ -0,0 +1,64 @@
+#include <iostream> 
+#include <string> 
+#include <cmath>
+using namespace std; 
+
+void getPassword() 
+{ 
+	int i;
+
+	for (i = 0; i != 3, i++;)
+	{ 
+		string password; 
+		cout << "Enter the password: "; 
+		getline(cin, password); 
+  
+		if (password == "12345") break; 
+			
+		else
+			cout << "INVALID. \n\n"; 
+		
+	} // while 
+} // getPassword 
+
+
+  
+int main() 
+{
+getPassword();
+	
+int i;
+i = 0;
+
+while (true)
+	if (i == 3) break;
+
+	int borrowed;
+	double rate; 	
+	
+	cout << endl;
+	cout << endl;
+	cout << "What's the amount borrowed: ";
+	cin >> borrowed;
+
+	
+	cout << "What's the annual interest rate: ";
+	cin >> rate;
+	cout << endl;
+	cout << endl;
+	
+	double mortgage;
+	double r = (rate/100) / 12;
+	int payback = 30;
+	int n = payback * 12;
+	
+	mortgage = (borrowed * pow(1+r, n) * r)/( pow(1+r, n) - 1);
+	
+	cout << "Amount borrowed: " << borrowed << endl;
+	cout << "Annual interest rate: " << rate << endl;
+	cout << "Payback period: " << payback << " years" << endl;
+	cout << "Monthly payback: $" << mortgage << endl;
+	
+
+	
+	}
