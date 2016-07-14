7310aab939f27ecb27c1fbd6b9e12bba21b212ea
mkwok94 <m.kwok94@yahoo.com>
2016-07-13 23:33:40 -0700

    Removed while loop in int main and reformatted code

--- mortgageCalculator2.cpp
+++ mortgageCalculator2.cpp
@@ -28,35 +28,33 @@ int getPassword()
 
   
 int main() 
-{
-while (true)
-	if (i == 3) break;
+	{
 
-	int borrowed;
-	double rate; 	
-	
-	cout << endl;
-	cout << endl;
-	cout << "What's the amount borrowed: ";
-	cin >> borrowed;
+		int borrowed;
+		double rate; 	
+		
+		cout << endl;
+		cout << endl;
+		cout << "What's the amount borrowed: ";
+		cin >> borrowed;
 
 	
-	cout << "What's the annual interest rate: ";
-	cin >> rate;
-	cout << endl;
-	cout << endl;
+		cout << "What's the annual interest rate: ";
+		cin >> rate;
+		cout << endl;
+		cout << endl;
 	
-	double mortgage;
-	double r = (rate/100) / 12;
-	int payback = 30;
-	int n = payback * 12;
+		double mortgage;
+		double r = (rate/100) / 12;
+		int payback = 30;
+		int n = payback * 12;
 	
-	mortgage = (borrowed * pow(1+r, n) * r)/( pow(1+r, n) - 1);
+		mortgage = (borrowed * pow(1+r, n) * r)/( pow(1+r, n) - 1);
 	
-	cout << "Amount borrowed: " << borrowed << endl;
-	cout << "Annual interest rate: " << rate << endl;
-	cout << "Payback period: " << payback << " years" << endl;
-	cout << "Monthly payback: $" << mortgage << endl;
+		cout << "Amount borrowed: " << borrowed << endl;
+		cout << "Annual interest rate: " << rate << endl;
+		cout << "Payback period: " << payback << " years" << endl;
+		cout << "Monthly payback: $" << mortgage << endl;
 	
 
 	
