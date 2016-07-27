119c9c047d049b594520b617b7afc183b3bac1ab
mkwok94 <m.kwok94@yahoo.com>
2016-07-27 12:02:22 -0700

    Add files via upload

--- Final Project Part2.cpp
+++ Final Project Part2.cpp
@@ -1,32 +1,7 @@
 #include <fstream>
 #include <iostream>
-#include <cctype>
 
 using namespace std;
-
-bool validation(char c) //True/False Function
-{
-	if(toupper(c) >= 'A' && toupper(c) <= 'Z') // Check it it has letters
-		{
-			return true;
-		}
-
-	else if (c >= '0' && c <= '9') // Check if it has numbesr
-		{
-			return true;
-		}
-
-	else if (c == '_' || c == '-' || c == '.' || c == '+' || c == '@') // Check for specific characters
-		{
-			return true;
-		}
-	
-	else
-		{
-			return false;
-		}
-}
-
 
 int main()
 {
@@ -38,7 +13,7 @@ int main()
 	string outputFile;
 	string defaultOutput = "copyPasteMyEmails.txt"; //Default
 
-	//User put in Input	
+	//User put in Input		
 	cout << "Enter input filename [default: " << defaultInput << "]: ";
 	getline(cin, userInput);
 	
@@ -53,7 +28,7 @@ int main()
 			defaultOutput = userInput; //User input --> Default Output
 		}
 	
-	//User put in Output		
+	//User put in Output	
 	cout << "Enter output filename [default: " << defaultOutput << "]: ";
 	getline(cin, userOutput);
 	
@@ -74,11 +49,12 @@ int main()
 	cout << "Input file: " << inputFile << endl;
 	cout << "Output file: " << outputFile << endl;
 	
+	
 	//Begin reading text files
 	ifstream fin;
    	fin.open(inputFile.c_str());
-    if(!fin.good()) throw "I/O error"; // Place error check 
-	
+    if(!fin.good()) throw "I/O error"; // Place error check
+ 
 	
 	//Read length of lines in file
 	string readFile;
@@ -88,24 +64,10 @@ int main()
         getline(fin, readFile);
         for(int i = 0; i < readFile.size() ;i++ ) // Read file lines
         {
-            if (readFile.at(i) == '@') // Looking for @ in email names
+            if (readFile.at(i) == '@')
 				{
-					int s, e, hasDot;
-					
-					for(s=i; validation(readFile.at(s)); s--); // Start validation
-					for(e=i; validation(readFile.at(e)); e++); // End validation
-					
-					if (readFile.at(e) == '.') // End looking for '.'
-						{
-							hasDot++;
-						}
-					
-					if (readFile.at(e) >= 1) // Print line when @ is found but pull as substing
-						{
-							cout << readFile.substr(s+1, e-s-1) << endl;
-						}
-					
-             	} // End of Line Check
+					cout<<readFile<<endl; // If line has '@' --> Print line
+             	}
 
         } // End for - read file
 
